d7be616f0ab7c8a463d9c559339e8dbadbe157ba
carolinev08 <164892775+carolinev08@users.noreply.github.com>
2025-04-08 13:35:11 -0300

    .

--- script.js
+++ script.js
@@ -34,3 +34,15 @@ botaoDeAcessibilidade.addEventListener('click', function (){
    })
    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
 const opcoesDeAcessibilidade = document.g
+const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
